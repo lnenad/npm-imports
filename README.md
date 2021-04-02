427f6eedfe8870ef2b018fc617ab1475c1a7515b
Nenad Lukic <nenad@happiestbaby.com>
2021-04-02 17:14:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# NPM Imports
+
+**npm imports** provides completions for npm dependencies found in your `package.json`.
+
+![](Screenshots/import.png)
+![](Screenshots/require.png)
+
+### Configuration
+
+You can configure whether the extension uses a single quote or a double quote when inserting package names in **Project → Project Settings...**
+
+## Contributors
+
+[lnenad](https://github.com/lnenad)
+
+## Sponsors
+
+[Mockadillo](https://mockadillo.com)
---
+++ Scripts/main.js
@@ -0,0 +1,100 @@
+const packageFileName = nova.path.join(nova.workspace.path, "package.json");
+let quoteConfig = nova.workspace.config.get(
+  "org.npmimports.quote-configuration"
+);
+let timer = null;
+
+const loadPackages = (fileContents) => {
+  const packageContents = JSON.parse(fileContents);
+  const dependencies = packageContents.dependencies
+    ? packageContents.dependencies
+    : [];
+  const devDependencies = packageContents.devDependencies
+    ? packageContents.devDependencies
+    : [];
+  const allDeps = [
+    ...Object.keys(dependencies),
+    ...Object.keys(devDependencies),
+  ];
+
+  const quoteSymbol = quoteConfig === "Single Quote" ? "'" : '"';
+
+  const items = new Set();
+  for (let i = 0; i < allDeps.length; i++) {
+    const pckg = allDeps[i];
+
+    let item = new CompletionItem(pckg, CompletionItemKind.Package);
+
+    item.insertText = `${quoteSymbol}${pckg}${quoteSymbol}`;
+
+    item.insertTextFormat = InsertTextFormat.Snippet;
+
+    items.add(item);
+  }
+
+  return items;
+};
+
+exports.activate = function () {
+  timer = setInterval(() => {
+    if (!nova.fs.stat(packageFileName)) {
+      console.log("No package.json found in workspace");
+      return;
+    }
+
+    clearInterval(timer);
+    timer = null;
+  }, 2000);
+
+  const fileHandle = nova.fs.open(packageFileName, "r");
+  const contents = fileHandle.read();
+  let packageList = loadPackages(contents);
+
+  watcher = nova.fs.watch(packageFileName, (filename) => {
+    console.log("Package.json change detected");
+    try {
+      const updated = fileHandle.read();
+
+      if (updated) {
+        packageList = loadPackages(updated);
+      }
+    } catch (e) {
+      console.error(`Error while parsing package.json at ${filename}: ${e}`);
+    }
+  });
+
+  nova.assistants.registerCompletionAssistant(
+    "*",
+    new CompletionProvider(packageList)
+  );
+};
+
+exports.deactivate = function () {
+  if (timer) {
+    clearInterval(timer);
+  }
+};
+
+class CompletionProvider {
+  constructor(packages) {
+    this.packages = [...packages];
+  }
+
+  packages = [];
+
+  importRegex = /(^import[\w\s\{\}]+from [\w\-\"]+)/;
+  requireRegex = /(require[\(\"\w]+)/;
+
+  provideCompletionItems(editor, context) {
+    const tstr = Date.now();
+    const line = context.line.trim();
+    const words = line.split(" ");
+    const text = words[words.length - 1];
+    let items = [];
+    if (this.importRegex.test(line) || this.requireRegex.test(line)) {
+      items = this.packages;
+    }
+
+    return items;
+  }
+}
